e93b0ec0152788d74f068f7d47d9617e4843853b
Kaeli Dalton <Kaelidalton@ufl.edu>
2023-01-28 17:52:53 -0500

    Questions array and run quiz function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+var questionsArr = [
+    {question:'JavaScript is the best language.',
+    answer: true,},
+    {question:'There are seven days in a week.',
+    answer: true,},
+    {question:'July has 30 days.',
+    answer: false,},
+    {question:'7x7 is 49.',
+    answer: true,},
+    {question:'11x11 is 111.',
+    answer: false,},
+]
+var numOfAgrees = 0
+function runQuiz(){
+}
