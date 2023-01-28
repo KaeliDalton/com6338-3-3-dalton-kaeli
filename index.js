12210e1bba1d408d877e4ae863d847c470568093
Kaeli Dalton <Kaelidalton@ufl.edu>
2023-01-28 18:32:42 -0500

    Scoring system attempt 1

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 var questionsArr = 
 [
     {question:'JavaScript is the best language.',
-    answer: true,},
+    answer: true},
 
     {question:'There are seven days in a week.',
-    answer: true,},
+    answer: true},
 
     {question:'July has 30 days.',
-    answer: false,},
+    answer: false},
 
     {question:'7x7 is 49.',
-    answer: true,},
+    answer: true},
 
     {question:'11x11 is 111.',
-    answer: false,},
+    answer: false},
 ]
-var numOfAgrees = 0
+let numOfAgrees = 0
+
 function runQuiz(){
-    for( var i=0; i < questionsArr.length; i++){
-     if(confirm(questionsArr[i].question) === (questionsArr[i].answer)) {
+    for( i=0; i < questionsArr.length; i++){
+     if(confirm(questionsArr[i].question) === questionsArr[i].answer) {
             numOfAgrees++
-        }  
+        }  else{}
     }
+    var score = (numOfAgrees/questionsArr.length) * 100
+    alert ('You Scored '+ Math.round(score) + '%!')
 }
