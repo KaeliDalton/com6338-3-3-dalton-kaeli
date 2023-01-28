b1c9f6a2dbc3696cd2b2f317d99449ae58e0b0ba
Kaeli Dalton <Kaelidalton@ufl.edu>
2023-01-28 18:22:50 -0500

    run quiz questions

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-var questionsArr = [
+var questionsArr = 
+[
     {question:'JavaScript is the best language.',
     answer: true,},
+
     {question:'There are seven days in a week.',
     answer: true,},
+
     {question:'July has 30 days.',
     answer: false,},
+
     {question:'7x7 is 49.',
     answer: true,},
+
     {question:'11x11 is 111.',
     answer: false,},
 ]
 var numOfAgrees = 0
 function runQuiz(){
+    for( var i=0; i < questionsArr.length; i++){
+     if(confirm(questionsArr[i].question) === (questionsArr[i].answer)) {
+            numOfAgrees++
+        }  
+    }
 }
